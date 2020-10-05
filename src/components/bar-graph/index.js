99a4d90bf888063bff22cbc3997d43af95d17594
Nicole Chow <nicolechowt@gmail.com>
2020-10-04 23:48:04 -0700

    Formatted bar graphs

--- src/components/bar-graph/index.js
+++ src/components/bar-graph/index.js
@@ -2,48 +2,62 @@ import React, {useEffect} from 'react';
 import * as d3 from "d3";
 
 function BarGraph(props) {
-  const { color, data, divideByTen } = props;
+  const { color, data } = props;
 
   useEffect(()=>{
     if(data.length>0) {
-      const w = 400;
-      const h = 300;
+      const width = 300;
+      const height = 350;
       
+      const margin = ({top: 30, right: 0, bottom: 30, left: 20});
+      const xAxisTickNames = ['M', 'T', 'W', 'T', 'F', 'S', 'S', 'M'];
+
+      const xAxis = g => g
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).tickFormat(i => xAxisTickNames[i]).tickSizeOuter(0))
+
+      const yAxis = g => g
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y).ticks(2))
+      .call(g => g.append("text")
+        .attr("x", -margin.left)
+        .attr("fill", "currentColor")
+        // .attr("text-anchor", "start")
+        .text(data)
+      )    
+
+      const x = d3.scaleBand()
+      .domain(d3.range(data.length))
+      .range([margin.left, width - margin.right])
+      .padding(0.3)
+
+      const y = d3.scaleLinear()
+      .domain([0, d3.max(data)]).nice()
+      .range([height - margin.bottom, margin.top])
+
       const svg = d3.select("#chart")
       .append("svg")
-      .attr("width", w)
-      .attr("height", h)
+      .attr("viewBox", [0, 0, width, height])
+      .attr("width", width)
+      .attr("height", height)
       .style("margin-left", 10);
-                    
-      svg.selectAll("rect")
-        .data(data)
-        .enter()
-        .append("rect")
-        .attr("x", (d, i) => i * 40)
-        .attr("y", (d, i) => {
-          if(!divideByTen){
-            return h - 10 * d
-          }
-          return h-3*d;
-        })
-        .attr("width", 25)
-        .attr("height", (d, i) => {
-          if(!divideByTen) {
-            return d * 10;
-          } 
-          return d*5;
-        })
-        .attr("fill", color)
-  
-        svg.selectAll("text")
-        .data(data)
-        .enter()
-        .append("text")
-        .text((d) => d)
-        .attr("x", (d, i) => i * 40)
-        .attr("y", (d, i) => h - (10 * d) - 3);
-    }
 
+      svg.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => x(i))
+      .attr("y", d => y(d))
+      .attr("height", d => y(0) - y(d))
+      .attr("width", x.bandwidth())
+      .attr("fill", color)
+
+      svg.append("g")
+      .call(xAxis);
+
+      svg.append("g")
+      .call(yAxis);  
+    }
   }, [color, data])
 
   if(!data) return;
