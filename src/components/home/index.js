77e906db839bb96c60cca2c7446c320fa74fd35e
Nicole Chow <nicolechowt@gmail.com>
2020-10-14 21:30:33 -0700

    README and splash screen

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-import Button from '../button';
+import { withRouter } from "react-router-dom";
 import './style/home.css';
 
-export default function Home() {
+function Home(props) {
+  const nextPath = (path) => {
+    props.history.push(path);
+  }
+
   return (
     <div className="home">
-      <h2>Tatas</h2>
-      <Button to="/create-account">Create Account</Button>
+      <h2>Perky Pals</h2>
 
-      <Button to="/login">Login</Button>
+      <div className="home__perky">
+        <div className="home__meetgoals">
+          MEET GOALS.
+        </div> 
+        
+        <div className="home__earn">
+          EARN COOL STUFF.
+        </div>
+      </div>
+
+
+      <button
+        className="button--pill-yellow"
+        onClick={()=>{nextPath('/dashboard')}}
+      >
+        Login
+      </button>
     </div>
   );
 }
+
+export default withRouter(Home);
